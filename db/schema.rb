c7d48743df4f0e4340e3f1daa91be1312bf6ddbe
ChristophMartinez <christophermartinez9@my.unt.edu>
2018-10-12 19:23:01 -0500

    new update

--- db/schema.rb
+++ db/schema.rb
@@ -12,18 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2018_10_08_202618) do
 
-  create_table "humen", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_humen_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_humen_on_reset_password_token", unique: true
-  end
-
   create_table "ingredients", force: :cascade do |t|
     t.text "content"
     t.integer "recipe_id"
@@ -32,25 +20,12 @@ ActiveRecord::Schema.define(version: 2018_10_08_202618) do
     t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
   end
 
-  create_table "installs", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_installs_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_installs_on_reset_password_token", unique: true
-  end
-
   create_table "recipes", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
-    t.integer "humen_id"
   end
 
   create_table "steps", force: :cascade do |t|
@@ -62,11 +37,15 @@ ActiveRecord::Schema.define(version: 2018_10_08_202618) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_hash_"
-    t.string "pasword_salt"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
